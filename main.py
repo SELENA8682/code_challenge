51884219e7927402ec2544feb3c827f524887815
石婉蓁 <selena8628@gmail.com>
2022-04-14 17:43:54 +0800

    refactor main.py add pydantic change input.json

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import json
-
-
-
-# naming
-# refactory remodule
+from typing import Optional
+from pydantic import BaseModel
 
 
 class Employee:
-    def __init__(self, employee_list: dict) -> None:
-        self.id = employee_list["id"]
-        self.name = employee_list['first_name']
-        self.manager = employee_list['manager']
-        self.salary = employee_list['salary']
+    def __init__(self, employee_dict: dict) -> None:
+        self.id = employee_dict["id"]
+        self.name = employee_dict['first_name']
+        self.manager = employee_dict['manager']
+        self.salary = employee_dict['salary']
         self.create_employees_list = []  # [id, name]
-        print(f"Created {self.name}")
 
     def sort_emp(self):
-        self.create_employees_list = sorted(self.create_employees_list, key= lambda x : x[1])
+        self.create_employees_list = sorted(
+            self.create_employees_list, key=lambda x: x[1])
 
-        
 
-# global
+class EmployeeJson(BaseModel):
+    id: int
+    first_name: str
+    manager: Optional[int] = None
+    salary: int
+
+
 staffs = {}
 
 
@@ -30,7 +32,7 @@ def hierarchy(id, count):
     print(slash + add.name)
     # print(s.employees)
 
-    if add.employees:  #dont use recursive function
+    if add.employees:  # dont use recursive function
         print(slash + f"Employees of: {add.name}")
         for e, _ in add.employees:
             hierarchy(e, count+1)
@@ -39,41 +41,44 @@ def hierarchy(id, count):
 def get_employee_list():
     file = open("input.json")
     employee_list = json.load(file)
+
+    new_employee_list = []
+    for employee in employee_list:
+        EmployeeJson(**employee)
+        new_object_in_employee_dict = Employee(employee)
+        staffs[new_object_in_employee_dict.id] = new_object_in_employee_dict
+        new_employee_list.append(new_object_in_employee_dict.id)
+
     file.close()
-    return employee_list
+    return new_employee_list
 
 
 def main():
     employee_list = get_employee_list()
-
-    # create object into dict id -> object
-    for employees in employee_list:  
-        new_object_in_employee_list_dic = Employee(employees)
-        staffs[new_object_in_employee_list_dic.id] = new_object_in_employee_list_dic
-        # print(s.id)
-
+    print(len(employee_list))
     # append employees
-    manager_id = 0  # assume there is no top manager
-    for id in staffs:
-        new_object_in_employee_list_dic = staffs[id]
-        if new_object_in_employee_list_dic.manager:
-            staffs[new_object_in_employee_list_dic.manager].employees.append([new_object_in_employee_list_dic.id, new_object_in_employee_list_dic.name])
-        else:
-            manager_id = new_object_in_employee_list_dic.id
+   
+    # manager_id = 0  # assume there is no top manager
+    # for id in staffs:
+    #     new_object_in_employee_list_dic = staffs[id]
+    #     if new_object_in_employee_list_dic.manager:
+    #         staffs[new_object_in_employee_list_dic.manager].employees.append([new_object_in_employee_list_dic.id, new_object_in_employee_list_dic.name])
+    #     else:
+    #         manager_id = new_object_in_employee_list_dic.id
 
-    #   sum up sealary simutiously
-    total_salary = 0
-    for id in staffs:
-        new_object_in_employee_list_dic = staffs[id]
-        new_object_in_employee_list_dic.sort_emp()
-        total_salary += new_object_in_employee_list_dic.salary
+    # #   sum up sealary simutiously
+    # total_salary = 0
+    # for id in staffs:
+    #     new_object_in_employee_list_dic = staffs[id]
+    #     new_object_in_employee_list_dic.sort_emp()
+    #     total_salary += new_object_in_employee_list_dic.salary
 
-    # printing staffs tree
-    hierarchy(manager_id, 0)
-    print(f"total salary: {total_salary}")
+    # # printing staffs tree
+    # hierarchy(manager_id, 0)
+    # print(f"total salary: {total_salary}")
 
-    # unit test
-    hierarchy(6, 0)  # id = 6
+    # # unit test
+    # hierarchy(6, 0)  # id = 6
 
 
 if __name__ == '__main__':
