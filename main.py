19dade11dd8efd3bbc2ae22a8f17e83ea6457171
石婉蓁 <selena8628@gmail.com>
2022-04-18 15:56:54 +0800

    delete hierarchy and add totalsalary

--- main.py
+++ main.py
@@ -3,61 +3,26 @@ import json
 from employee_config import Employee, EmployeeJson
 
 
-staffs = {}
-
-
-def hierarchy(id, count):
-    add = staffs[id]
-    slash = " \t " * count
-    print(slash + add.name)
-    # print(s.employees)
-
-    if add.create_employees_list:  # dont use recursive function
-        print(slash + f"Employees of: {add.name}")
-        for e, _ in add.create_employees_list:
-            hierarchy(e, count+1)
-
-
 def get_employee_list():
     file = open("input.json")
     employee_list = json.load(file)
 
     new_employee_list = []
     for employee in employee_list:
-        new_object_in_employee_dict = EmployeeJson(**employee).get_employee()
-        staffs[new_object_in_employee_dict.id] = new_object_in_employee_dict
-        new_employee_list.append(new_object_in_employee_dict.id)
+        employee_instance = EmployeeJson(**employee).get_employee()
+        new_employee_list.append(employee_instance)
 
     file.close()
     return new_employee_list
 
+def get_total_salary(employee_list):
+    return sum(em.salary for em in employee_list)
+  
+
 
 def main():
     employee_list = get_employee_list()
-    print(len(employee_list))
-
-    # manager_id = None  # assume there is no top manager
-    # for id in staffs:
-    #     new_object_in_employee_dict = staffs[id]
-    #     if new_object_in_employee_dict.manager:
-    #         staffs[new_object_in_employee_dict.manager].create_employees_list.append(
-    #             [new_object_in_employee_dict.id, new_object_in_employee_dict.name])
-    #     else:
-    #         manager_id = new_object_in_employee_dict.id
-
-    # #   sum up sealary simutiously
-    # total_salary = 0
-    # for id in staffs:
-    #     new_object_in_employee_dict = staffs[id]
-    #     new_object_in_employee_dict.sort_emp()
-    #     total_salary += new_object_in_employee_dict.salary
-
-    # # printing staffs tree
-    # hierarchy(manager_id, 0)
-    # print(f"total salary: {total_salary}")
-
-    # # unit test
-    # hierarchy(6, 0)  # id = 6
+    totalsalary = get_total_salary(employee_list)   
 
 
 if __name__ == '__main__':
