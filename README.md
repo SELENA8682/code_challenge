251a2cff5e002b5c8543b7ab2814eb1f4e3d1567
SELENA8682 <102606262+SELENA8682@users.noreply.github.com>
2022-04-25 12:29:30 +0800

    Docstring employee config (#1)
    add docstring in employee_config.py
    
    1.class
    2.function get_manager/set_manager in class Employee
    3.function get_employee in class EmployeeJson(BaseModel)

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # code challenge
+## Enviroment
+- python
+- pydantic
+- pytest
+
+##  Files
+
+- main.py : main excute code
+- employee_cogfig.py : claa Employee and Pydantic
+- input.json : loading employee data in json format
+- test_main : test the function with pytest
+- README.md : explanation of all the project in high level
+- gitignore : to ask for not showing certain file or funtion
+
+
+## Work flow
+
+### 1.load json file (input.json) 
+### 2.define Enployee class and EmployeeJson(BaseModel) class for pydantic
+### 3.create employee_list to store json data
+### 4.create new_employee_list to get employee data from EmployeeJson which transfer from employee_list
+### 5.define fuction get_total_salary from employee_list
+### 6.to match manager from employee_dict by manager id
--- employee_config.py
+++ employee_config.py
@@ -1,20 +1,66 @@
 from typing import Optional
 from pydantic import BaseModel
 
-
 class Employee:
+    """
+    This is a class for all the employees .
+      
+    Attributes:
+    -----------
+        id (int): The id number of each employee.
+        first_name (str):The first name of each employee.
+        manager (int): To show whether employee has manager, if it has manager reveals the manager id ; while shows  null
+        salary (int): salary of each employee.
+    """
     manager_instance: 'Employee'
-
+   
     def __init__(self, id, first_name ,manager, salary) :
+        """
+        Construct all the necessary attributes for the employee object.
+
+        Parameters
+        ---
+            id (int): The id number of each employee.
+            first_name (str):The first name of each employee.
+            manager (int): To show whether employee has manager, if it has manager reveals the manager id ; while shows  null
+            salary (int): salary of each employee.
+        
+        Methods
+        ---
+            get_manager():
+                to load manager's instance from employee
+
+            set_manager(manager):
+                 to link employee and manager
+        """
         self.id = id
         self.name = first_name
         self.manager = manager
         self.salary = salary
     
     def get_manager(self) -> 'Employee':
+        """
+            to get manager info 
+
+             Returns
+             -------
+                manager_instance: 
+                    the variable in Employee to put manager info
+        """       
         return self.manager_instance
+       
     def set_manager(self,manager):
+        """
+        to link employee and manager
+
+        Parameters
+        ----------
+        manager : 
+            link the manager info in to manager instance
+        """
         self.manager_instance = manager
+
+        
 
 #DTO data transfer object
 class EmployeeJson(BaseModel):
@@ -24,4 +70,12 @@ class EmployeeJson(BaseModel):
     salary: int
 
     def get_employee(self):
+        """
+            to get all the employee info 
+
+             Returns
+             -------
+               return all the parameters in class EmployeeJson(BaseModel)
+        """       
         return Employee(self.id, self.first_name, self.manager, self.salary)
+        
