b7b4be3e8f5d471e0c324ad950d83e0dc461f56c
KB <kb19900709@gmail.com>
2022-04-26 18:28:19 +0800

    bind the relationship between Manager and Employee
    1. set an empty manager dict
    2. add a if condition for whether create the manager or not
    3. invoke set_manager in Employee, which will add the employee instance in the employees in Manager

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import json
 from typing import List
 
-from employee_config import Employee, EmployeeJson
-
-
+from employee_config import Employee, EmployeeJson, Manager
 
 
 def get_employee_list(file_name) -> List[Employee]:
     if not file_name:
         return []
-        
+
     file = open(file_name)
     employee_list = json.load(file)
     new_employee_list = []
@@ -20,24 +18,38 @@ def get_employee_list(file_name) -> List[Employee]:
     file.close()
     return new_employee_list
 
+
 def get_total_salary(employee_list):
     if employee_list:
         return sum(em.salary for em in employee_list)
-    return 0 
+    return 0
+
 
 def main():
-    employee_list = get_employee_list("input.json") 
-    
-    employee_dict =  {}
-    for employee in employee_list: 
-        employee_dict[employee.id] = employee
-            
+    employee_list = get_employee_list("input.json")
+
+    employee_dict = {}
     for employee in employee_list:
-           if employee.manager:
-                manager = employee_dict.get(employee.manager)
-                employee.set_manager(manager)
-       
-    totalsalary = get_total_salary(employee_list)   
+        employee_dict[employee.id] = employee
+
+    manager_dict = {}
+    for employee in employee_list:
+        manager_id = employee.manager
+        if not manager_id:
+            continue
+
+        manager = None
+
+        if manager_id in manager_dict:
+            manager = manager_dict[manager_id]
+        else:
+            employee_m = employee_dict.pop(manager_id)
+            manager = Manager(employee_m)
+            manager_dict[manager_id] = manager
+
+        employee.set_manager(manager)
+
+    totalsalary = get_total_salary(employee_list)
 
 
 if __name__ == '__main__':
