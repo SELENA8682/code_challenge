225eb92542b78a61336e1dff55f8691772155c18
selena <selena8628@gmail.com>
2022-05-23 10:22:55 +0800

    change function to object and add error handling

--- package/total_salary.py
+++
@@ -1,7 +0,0 @@
-def get_total_salary(employee_list):
-    try:
-        if employee_list:
-            return sum(em.salary for em in employee_list)
-    except TypeError:
-        print("salary should be integer")
-    return 0
